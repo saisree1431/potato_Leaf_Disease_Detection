0cad5ed331b7447b5924c26fca3837c5a9db3031
Saisri Ujjuri <saisriujjuri@gmail.com>
2025-11-16 15:57:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import tensorflow as tf
 import numpy as np
 def model_prediction(test_image):
     model = tf.keras.models.load_model("trained_plant_disease_model.keras")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    
+    image = Image.open(test_image)
+    image = image.resize((128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr]) #convert single image to batch
+    input_arr = np.expand_dims(input_arr, axis=0)
+    
     predictions = model.predict(input_arr)
-    return np.argmax(predictions) #return index of max element
+    return np.argmax(predictions)
 
 #Sidebar
 st.sidebar.title("Plant Disease Detection System for Sustainable Agriculture")
@@ -37,6 +40,7 @@ elif(app_mode=="DISEASE RECOGNITION"):
         st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
+
         #Reading Labels
         class_name = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
